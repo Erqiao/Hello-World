478fb1ed3c3eeeb46479b503ad391dc1028a3cb8
Erqiao <36697117+Erqiao@users.noreply.github.com>
2018-02-26 01:34:30 -0500

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,18 @@
+given_list = [4, 3, 2, -1, 5, 6,-3,-9, -4]
+total = 0
+
+for element in given_list:
+    if element < 0:
+        continue
+    total += element
+print total 
+
+total1 = 0
+i = 0
+while True:
+    if given_list[i] < 0
+        continue
+    total1 += given_list[i]
+    i += 1
+print total1
+    
