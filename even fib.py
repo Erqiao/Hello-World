40508ba2801f3e5913d67f82fb53a404fe1b02fe
Erqiao <36697117+Erqiao@users.noreply.github.com>
2018-02-28 22:07:16 -0500

    Create even fib.py

---
+++ even fib.py
@@ -0,0 +1,15 @@
+fiblist = [1,2]
+evenfiblist = []
+a = 0
+i = 0
+
+while fiblist[i] < 4000000:
+    if fiblist[i+1]%2 ==0:
+        evenfiblist.append(fiblist[i+1])
+        print evenfiblist
+        a += fiblist[i+1]
+    fiblist.append(fiblist[i] + fiblist[i+1])
+    i += 1
+    
+print a, evenfiblist
+
